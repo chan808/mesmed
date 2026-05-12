257357287b6b16364d614c3dd0c9d8c2c57a7c43
chan808 <rlackswhd200@gmail.com>
2026-05-12 17:54:14 +0900

    init

---
+++ src/main/java/com/chan/medmes/MedmesApplication.java
@@ -0,0 +1,13 @@
+package com.chan.medmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedmesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedmesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chan/medmes/MedmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan.medmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedmesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
